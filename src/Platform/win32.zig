ee50b957cdbf6067915ddcb459f8b8b8cfeceef3
Quentin_T <quentin.tessier@epitech.eu>
2023-11-15 18:10:05 +0100

    adding a gitignore

--- src/Platform/win32.zig
+++ src/Platform/win32.zig
@@ -1,2 +1,33 @@
 const std = @import("std");
-const zigwin32 = @import("zigwin32");
+const win32 = @import("zigwin32").everything;
+
+pub const Window = struct {
+    name: [*:0]const u8,
+    extent: struct {
+        x: i32,
+        y: i32,
+        width: i32,
+        height: i32,
+    },
+
+    // Win32 specific
+    hInstance: win32.HINSTANCE = undefined,
+    hwnd: win32.HWND = undefined,
+
+    pub fn init(name: [*:0]const u8, width: i32, height: i32) Window {
+        return .{ .name = name, .extent = .{
+            .x = 0,
+            .y = 0,
+            .width = width,
+            .height = height,
+        } };
+    }
+
+    pub fn create(self: *Window) void {
+        // TODO: Check for error
+        self.hInstance = win32.GetModuleHandleA(null) orelse unreachable;
+
+        var bBrush = win32.GetStockObject(win32.BLACK_BRUSH);
+        _ = bBrush;
+    }
+};
--- src/main.zig
+++ src/main.zig
@@ -1,7 +1,27 @@
 const std = @import("std");
+const builtin = @import("builtin");
 
 pub const Platform = enum {
     Windows,
     Wayland,
     X11,
 };
+
+pub fn PlatfromWindow() type {
+    switch (builtin.os.tag) {
+        .windows => {
+            const Win32Window = @import("Platform/win32.zig").Window;
+            return Win32Window;
+        },
+        else => {
+            @panic("Lib only supports Windows right now");
+        },
+    }
+}
+
+const Window = PlatfromWindow();
+
+pub fn main() anyerror!void {
+    var window = Window.init("test", 1, 1);
+    window.create();
+}
