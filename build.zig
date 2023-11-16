5600e958c58e07480084fd688c909d12626d92cc
Quentin_T <quentin.tessier@epitech.eu>
2023-11-16 14:09:22 +0100

    Fixing some issues with compilation

--- build.zig
+++ build.zig
@@ -2,10 +2,16 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) !void {
     const zigwin32 = b.dependency("zigwin32", .{});
-    _ = b.addModule("AntleneWindowSystem", .{ .source_file = .{
-        .path = "src/main.zig",
-    }, .dependencies = &.{.{
-        .name = "zigwin32",
-        .module = zigwin32.module("zigwin32"),
-    }} });
+    var module = b.createModule(.{
+        .source_file = .{
+            .path = "src/main.zig",
+        },
+        .dependencies = &.{
+            .{
+                .name = "zigwin32",
+                .module = zigwin32.module("zigwin32"),
+            },
+        },
+    });
+    try b.modules.put(b.dupe("AntleneWindowSystem"), module);
 }
